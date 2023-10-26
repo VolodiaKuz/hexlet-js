5d4d87962cc727a6096025301b77cb6970a94f26
Volodia K <wtraktorman94@mail.ru>
2023-10-26 18:14:45 +0600

    study lesson5 Завсисимости

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+import _ from "lodash";
+
+// Пересечение массивов
+console.log('пересечение массивов [2, 1], [2, 3]');
+console.log(_.intersection([2, 1], [2, 3])); // => [2]
+
+console.log("_.last(['one', 'two'])");
+console.log(_.last(['one', 'two']));
+
+// capitalize делает первую букву заглавной
+console.log(_.capitalize("hello")); // Hello
+
+
 console.log('Hello, Hexlet!');
 
