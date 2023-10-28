7c0dad01e9cefe50d5827fbc0dbd668240fd0107
Volodia K <wtraktorman94@mail.ru>
2023-10-28 15:53:49 +0600

    install prettier and use npx to start prettier

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import _ from "lodash";
 
 // Пересечение массивов
-console.log('пересечение массивов [2, 1], [2, 3]');
+console.log("пересечение массивов [2, 1], [2, 3]");
 console.log(_.intersection([2, 1], [2, 3])); // => [2]
 
 console.log("_.last(['one', 'two'])");
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
 
 // capitalize делает первую букву заглавной
 console.log(_.capitalize("hello")); // Hello
 
+console.log("Hello, Hexlet!");
 
-console.log('Hello, Hexlet!');
-
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
