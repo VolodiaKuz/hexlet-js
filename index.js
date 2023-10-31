cacdc462000f82457247ce0341c942f9891bb869
Volodia K <wtraktorman94@mail.ru>
2023-10-31 21:25:34 +0600

    fix errors with eslint

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-import _ from "lodash";
+import _ from 'lodash';
 
 // Пересечение массивов
-console.log("пересечение массивов [2, 1], [2, 3]");
+// console.log("пересечение массивов [2, 1], [2, 3]");
 console.log(_.intersection([2, 1], [2, 3])); // => [2]
 
-console.log("_.last(['one', 'two'])");
-console.log(_.last(["one", "two"]));
+// console.log("_.last(['one', 'two'])");
+console.log(_.last(['one', 'two']));
 
 // capitalize делает первую букву заглавной
-console.log(_.capitalize("hello")); // Hello
+console.log(_.capitalize('hello')); // Hello
 
-console.log("Hello, Hexlet!");
+// console.log("Hello, Hexlet!");
 
 const fn = (a, b) => {
   const c = a + b;
   return c;
 };
+
+console.log(fn(7, 7));
